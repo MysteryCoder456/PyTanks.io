752032eb175e107c87e11c79f061263460246bff
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-02-21 17:33:11 +0400

    Added pymunk code

--- scripts/ground.py
+++ scripts/ground.py
@@ -1,18 +1,37 @@
 import pygame
+import pymunk
 
 
 class Ground:
-    def __init__(self, window_width, window_height):
-        self.x = 0
-        self.y = window_height - 150
+    """Class for making the Ground/Terrain in the game
 
-        self.width = window_width
-        self.height = window_height - self.y
+    Arguments:
+        window_width {int} -- width of the game window
+        window_height {int} -- height of the game window
+    """
+
+    def __init__(self, width, height, window_size):
+        self.vertices = [
+            (0, 0),
+            (width, 0),
+            (width, height),
+            (0, height)
+        ]
+
+        self.body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        self.shape = pymunk.Poly(self.body, self.vertices)
         self.color = (0, 170, 0)
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+        self.body.position = 0, window_size[1] - height
+        self.update_rect()
+
+    def add_to_space(self, space):
+        space.add(self.body, self.shape)
+
+    def update_rect(self):
+        pos = self.body.position
+        size = self.vertices[2]
+        self.rect = pygame.Rect(pos[0], pos[1], size[0], size[1])
 
     def render(self, window):
         pygame.draw.rect(window, self.color, self.rect)
-
-    def collide(self, Rect):
-        return self.rect.colliderect(Rect)
--- scripts/main.py
+++ scripts/main.py
@@ -1,26 +1,39 @@
+"""
+This is a Tank Game based on PocketTanks, made in Python 3 using Pygame and Pymunk for physics
+"""
+
+import sys
+import pygame
+import pymunk
+import pymunk.pygame_util
+
 # CLASS IMPORTS
 from ground import Ground
 
-import pygame
-pygame.init()
-
 
 class PyTanksIO:
+    """
+    Main class that contains most of the code for the game
+    """
+
     def __init__(self, width, height, title):
         # Window initializing code
-        self.width = width
-        self.height = height
+        self.win_size = width, height
         self.win = pygame.display.set_mode((width, height))
         pygame.display.set_caption(title)
 
-        self.clock = pygame.time.Clock()
         self.running = True
-        self.FPS = 60
         self.background = (0, 0, 0)
 
-    def start(self):
+        # Pymunk initializing code
+        pymunk.pygame_util.positive_y_is_up = False
+        self.space = pymunk.Space()
+        self.space.gravity = 0, 100
+
         # Game initializing code
-        self.ground = Ground(self.width, self.height)
+        self.ground = Ground(self.win_size[0], 150, self.win_size)
+
+        self.ground.add_to_space(self.space)
 
     def logic(self):
         pass
@@ -34,15 +47,22 @@ class PyTanksIO:
 
 
 def main():
-    game = PyTanksIO(1024, 700, "PyTanks.io")
-    game.start()
+    """
+    The main function that starts the entire game.
+    Only modify this function if you know what you're doing.
+    """
 
-    while game.running:
-        game.clock.tick(game.FPS)
+    pygame.init()
+    game = PyTanksIO(1048, 700, "PyTanks.io")
+    clock = pygame.time.Clock()
+    fps = 60
+
+    while True:
+        clock.tick(fps)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                game.running = False
+                return
 
         game.logic()
         game.render()
@@ -50,5 +70,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    pygame.quit()
-    quit()
+    sys.exit()
